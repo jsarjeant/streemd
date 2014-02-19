3bf1c8cd388a88f96b95f5fd5446dfaeec129dd1
Robby Peters <rgpeters8@gmail.com>
2014-02-18 20:27:46 -0800

    Add Login activity

--- gen/com/example/streemd/R.java
+++ gen/com/example/streemd/R.java
@@ -24,11 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int passwordEditText=0x7f080001;
+        public static final int signInButton=0x7f080002;
+        public static final int usernameEditText=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int user_feed=0x7f030001;
+        public static final int login=0x7f030001;
+        public static final int user_feed=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -37,7 +41,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int password=0x7f050006;
+        public static final int sign_in=0x7f050004;
         public static final int test_string=0x7f050003;
+        public static final int user_login=0x7f050007;
+        public static final int username=0x7f050005;
     }
     public static final class style {
         /** 
