e4397f8b9cdc0e7cda52d9eb2a0d3b4a50f8f71c
Robby Peters <rgpeters8@gmail.com>
2014-02-18 21:36:40 -0800

    Further sign in functionality

--- gen/com/example/streemd/R.java
+++ gen/com/example/streemd/R.java
@@ -24,14 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int passwordEditText=0x7f080001;
-        public static final int signInButton=0x7f080002;
+        public static final int signInButton=0x7f080003;
+        public static final int testSignInButton=0x7f080002;
         public static final int usernameEditText=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int login=0x7f030001;
+        public static final int sign_in=0x7f030001;
         public static final int user_feed=0x7f030002;
     }
     public static final class menu {
@@ -44,6 +45,7 @@ public final class R {
         public static final int password=0x7f050006;
         public static final int sign_in=0x7f050004;
         public static final int test_string=0x7f050003;
+        public static final int test_user=0x7f050008;
         public static final int user_login=0x7f050007;
         public static final int username=0x7f050005;
     }
--- src/com/example/streemd/SignIn_VP.java
+++ src/com/example/streemd/SignIn_VP.java
@@ -1,12 +1,9 @@
 package com.example.streemd;
 
-import com.example.streemd.R;
-
-import edu.calpoly.android.lab3.Joke;
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
@@ -18,23 +15,26 @@ public class SignIn_VP extends Activity{
 	/** Button used for signing in */
 	protected Button m_vwSignInButton;
 	
+	/** Fills sign in fields with test data*/
+	protected Button m_vwTestCredsButton;
+	
 	/** Input for entering username */
-	protected EditText m_vwUsernameText;
+	protected EditText m_vwUsernameEditText;
 	
 	/** Input for entering password */
-	protected EditText m_vwPasswordText;
+	protected EditText m_vwPasswordEditText;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.sign_in);
+		
 		m_vwSignInButton = (Button) findViewById(R.id.signInButton);
-		m_vwUsernameText = (EditText) findViewById(R.id.usernameEditText);
-		m_vwPasswordText = (EditText) findViewById(R.id.passwordEditText);
+		m_vwTestCredsButton = (Button) findViewById(R.id.testSignInButton);
+		m_vwUsernameEditText = (EditText) findViewById(R.id.usernameEditText);
+		m_vwPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
 		
 		initSignInListener();
-		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
-		
 	}
 	
 	/**
@@ -44,19 +44,35 @@ public class SignIn_VP extends Activity{
 	protected void initSignInListener() {
 		m_vwSignInButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
-				Resources r = getResources();
-				String username = m_vwUsernameText.getText().toString();
-				String password = m_vwPasswordText.getText().toString();
+				String username = m_vwUsernameEditText.getText().toString();
+				String password = m_vwPasswordEditText.getText().toString();
 				
-				/*if(!jokeStr.equals("") && !jokeStr.equals(null)) {
-					m_vwJokeEditText.setText("");
-					addJoke(new Joke(jokeStr, r.getString(R.string.author_name)));
-				}*/
+				if(!username.equals("") && !username.equals(null) && !password.equals("") && !password.equals(null)) {
+					Log.d(null, "username = " + username);
+					Log.d(null, "password = " + password);
+					if(checkCredentials(username, password)) {
+						Log.d(null, "Valid user");
+					}
+					else {
+						Log.d(null, "Invalid user");
+					}
+				}
 				
-				/*InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-				imm.hideSoftInputFromWindow(m_vwJokeEditText.getWindowToken(), 0);*/
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(m_vwUsernameEditText.getWindowToken(), 0);
+				imm.hideSoftInputFromWindow(m_vwPasswordEditText.getWindowToken(), 0);
 			}
 		});
+		m_vwTestCredsButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				m_vwUsernameEditText.setText("exampleUser");
+				m_vwPasswordEditText.setText("foobar");
+			}
+		});
+	}
+	
+	public boolean checkCredentials(String username, String password) {
+		return false;
 	}
 
 }
